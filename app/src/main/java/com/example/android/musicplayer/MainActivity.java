9c90b50e5082c662725a6471562823123fa2b4e5
Sebastian John von Freyend <sjvf@Sebastians-MacBook-Pro.local>
2017-11-05 13:02:54 +0100

    feat: Added song data to the MainActivity.

--- app/src/main/java/com/example/android/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/android/musicplayer/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.musicplayer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Song data
+        ArrayList<Song> songList = new ArrayList<Song>();
+        songList.add(new Song("Title #1", "Artist"));
+        songList.add(new Song("Title #2", "Artist"));
+        songList.add(new Song("Title #3", "Artist"));
+        songList.add(new Song("Title #4", "Artist"));
+        songList.add(new Song("Title #6", "Artist"));
+        songList.add(new Song("Title #7", "Artist"));
+        songList.add(new Song("Title #8", "Artist"));
+        songList.add(new Song("Title #9", "Artist"));
+        songList.add(new Song("Title #10", "Artist"));
     }
 }
